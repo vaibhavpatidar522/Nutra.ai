4aa4cbbc5f8b3da6aae637dd55c237a59e948790
Vaibhav Patidar <vaibhavpatidar522@gmail.com>
2025-05-05 08:28:00 +0530

    Project Completed

--- convex/auth.config.ts
+++ convex/auth.config.ts
@@ -1,7 +1,7 @@
 export default {
   providers: [
     {
-      domain: "https://guiding-gobbler-84.clerk.accounts.dev/",
+      domain: "https://equal-lamb-88.clerk.accounts.dev",
       applicationID: "convex",
     },
   ],
